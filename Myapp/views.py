59fba9d359cfade2ce3d4210f5a4e6c584874356
Saksham <Saksham.khanal01@gmail.com>
2022-03-03 17:10:26 +0545

    After appending of forms

--- Myapp/views.py
+++ Myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .forms import ImageForm
+from .forms import form_render
 from .models import Image
 # Create your views here.
 def home(request):
-    form = ImageForm()
-    return render(request,'home.html')
+    form = form_render()
+    return render(request,'home.html',{'form':form})
